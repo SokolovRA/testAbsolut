eba54bd7444057893165cf28361c7d316cd98723
Roman Sokolov <119596832+SokolovRA@users.noreply.github.com>
2023-09-26 21:57:49 +0300

    done

--- src/main/java/org/example/ActionCounter.java
+++ src/main/java/org/example/ActionCounter.java
@@ -4,4 +4,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ActionCounter {
+    private Queue<Integer> queue;
+
+    public ActionCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public void call(int timestamp) {
+        queue.add(timestamp);
+        while (!queue.isEmpty() && timestamp - queue.peek() >= 300) {
+            queue.poll();
+        }
+    }
+
+    public int getActions(int timestamp) {
+        while (!queue.isEmpty() && timestamp - queue.peek() >= 300) {
+            queue.poll();
+        }
+        return queue.size();
+    }
 }
+
+
